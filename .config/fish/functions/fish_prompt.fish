b1022393f03470dd75889be0044aecbcd0030d1c
Yuya Nagai <ynny.opem@gmail.com>
2022-01-22 16:33:06 +0900

    prompt を変更

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,91 +1,21 @@
 function fish_prompt --description 'Write out the prompt'
-    # 環境に応じてプロンプトを分ける
-    # 環境によって Web で選択したデザインに含まれる関数が実行できない事がある
+    set -l last_pipestatus $pipestatus
+
+    # Time Zone settings
+    set -g theme_date_timezone Asia/Tokyo
+    set -g theme_date_format  "+%Y-%m-%d %H:%M"
+
     if is_wsl
-        set -l last_pipestatus $pipestatus
-
-        if not set -q __fish_git_prompt_show_informative_status
-            set -g __fish_git_prompt_show_informative_status 1
-        end
-        if not set -q __fish_git_prompt_hide_untrackedfiles
-            set -g __fish_git_prompt_hide_untrackedfiles 1
-        end
-        if not set -q __fish_git_prompt_color_branch
-            set -g __fish_git_prompt_color_branch magenta --bold
-        end
-        if not set -q __fish_git_prompt_showupstream
-            set -g __fish_git_prompt_showupstream "informative"
-        end
-        if not set -q __fish_git_prompt_char_upstream_ahead
-            set -g __fish_git_prompt_char_upstream_ahead "↑"
-        end
-        if not set -q __fish_git_prompt_char_upstream_behind
-            set -g __fish_git_prompt_char_upstream_behind "↓"
-        end
-        if not set -q __fish_git_prompt_char_upstream_prefix
-            set -g __fish_git_prompt_char_upstream_prefix ""
-        end
-        if not set -q __fish_git_prompt_char_stagedstate
-            set -g __fish_git_prompt_char_stagedstate "●"
-        end
-        if not set -q __fish_git_prompt_char_dirtystate
-            set -g __fish_git_prompt_char_dirtystate "✚"
-        end
-        if not set -q __fish_git_prompt_char_untrackedfiles
-            set -g __fish_git_prompt_char_untrackedfiles "…"
-        end
-        if not set -q __fish_git_prompt_char_invalidstate
-            set -g __fish_git_prompt_char_invalidstate "✖"
-        end
-        if not set -q __fish_git_prompt_char_cleanstate
-            set -g __fish_git_prompt_char_cleanstate "✔"
-        end
-        if not set -q __fish_git_prompt_color_dirtystate
-            set -g __fish_git_prompt_color_dirtystate blue
-        end
-        if not set -q __fish_git_prompt_color_stagedstate
-            set -g __fish_git_prompt_color_stagedstate yellow
-        end
-        if not set -q __fish_git_prompt_color_invalidstate
-            set -g __fish_git_prompt_color_invalidstate red
-        end
-        if not set -q __fish_git_prompt_color_untrackedfiles
-            set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-        end
-        if not set -q __fish_git_prompt_color_cleanstate
-            set -g __fish_git_prompt_color_cleanstate green --bold
-        end
-
-        set -l color_cwd
-        set -l prefix
-        set -l suffix
-        switch "$USER"
-            case root toor
-                if set -q fish_color_cwd_root
-                    set color_cwd $fish_color_cwd_root
-                else
-                    set color_cwd $fish_color_cwd
-                end
-                set suffix '#'
-            case '*'
-                set color_cwd $fish_color_cwd
-                set suffix '$'
-        end
-        # PWD
-        set_color $color_cwd
-        echo -n (prompt_pwd)
-        set_color normal
-
-        printf '%s ' (fish_vcs_prompt)
-
-        set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
-        echo -n $pipestatus_string
-        set_color normal
-
-        echo -n "$suffix "
-    else if is_remote_extension
-        printf 'container %s > ' (prompt_pwd)
-    else 
-        printf '%s@%s %s > ' $USER (prompt_hostname) (prompt_pwd)
+        set hn "wsl"
+    else if is_remote_container
+        set hn "container"
+    else if is_colab
+        set hn "colab"
+    else
+        set hn prompt_hostname
     end
+
+    printf "%s %s [%s]\n" $hn $USER (date "+%m/%d/%Y %H:%M")
+    printf '%s\n' (realpath .)
+    printf '> '
 end
